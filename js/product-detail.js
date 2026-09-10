1a86b765507a9ab5e29cb8ee7fa4ac571a43cdfb
InkyinKhin <inkyinkhin238@gmail.com>
2026-09-10 17:44:19 +0630

    adding size btn feature and stock and modify style

--- js/product-detail.js
+++ js/product-detail.js
@@ -33,12 +33,13 @@ const product = products.find(product => {
 console.log(productID);
 console.log(product);
 
+const breadcrumbProduct = document.querySelector("#breadcrumbProduct");
 const productImg = document.querySelector("#productImg");
 const productName = document.querySelector("#productName");
 const productDes = document.querySelector("#productDes");
 const productPrice = document.querySelector("#productPrice");
 
-
+breadcrumbProduct.textContent = product.name;
 productImg.src = product.mainImage;
 productImg.alt = product.name;
 productName.textContent = product.name;
@@ -46,6 +47,8 @@ productDes.textContent = product.description;
 productPrice.textContent = product.price + ` MMK`;
 
 
+let selectedVariant = null;
+let selectedSize = null;
 
 const colorOptions = document.querySelector("#colorOptions");
 
@@ -64,20 +67,68 @@ colorBtns.forEach(colorBtn => {
     colorBtn.addEventListener("click", () => {
         const selectedColor = colorBtn.dataset.color;
         console.log(selectedColor);
-        
-
-        const selectedVariant = product.variants.find( variant => {
+    
+        selectedVariant = product.variants.find( variant => {
         return variant.color === selectedColor;
         });
-
+    
+        selectedSize = null;
+        stockMessage.textContent = "";
+      
         productImg.src = selectedVariant.image;
+
+        sizeRender();
     });
     
 });
 
-
-
 const sizeOptions = document.querySelector("#sizeOptions");
+const stockMessage = document.querySelector("#stockMessage");
 
+function sizeRender(){
+
+    if(!selectedVariant) {
+        sizeOptions.innerHTML = `<p class="size-message">Choose a color to see available sizes</p>`;
+    }else{
+        const sizeButtons = selectedVariant.sizes.map(size => {
+            if(size.stock === 0){
+                return `<button class = "size-btn" data-size="${size.size}" disabled>${size.size}</button>`
+            }else
+                return `<button class = "size-btn" data-size="${size.size}">${size.size}</button>`
+        }).join(" ");
+        
+        sizeOptions.innerHTML = sizeButtons;
+
+        const sizeBtns = sizeOptions.querySelectorAll(".size-btn");
+
+        console.log(sizeBtns);
+
+        sizeBtns.forEach(sizeBtn => { 
+            sizeBtn.addEventListener("click", ()=> { 
+                const clickedSize = sizeBtn.dataset.size;  //this is the size when the customer click the size btn and change from DOM to js
+                console.log(clickedSize);
+
+            selectedSize = selectedVariant.sizes.find(size => {
+                return size.size === clickedSize;
+            })
+        
+             console.log(selectedSize);
+        
+            stockMessage.textContent = `Available Stock: ${selectedSize.stock}`;
+            console.log(stockMessage);
+        })
+})
+
+
+    }
+}
+
+    sizeRender();
+
+
+
+productFabric.textContent = product.fabric;
+productCare.textContent = product.care;
+productCategory.textContent =`Category:` + product.category;
 
     
